0310ff4c3050fe8b88a2f3ea47dd475b1e2faec4
Hanan Zawahra\thwaib <hanan.zawahra@harri.com>
2022-08-22 00:31:43 +0200

    Create Vehicle class

---
+++ Cars/Vehicle.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+public class Vehicle extends Automobile
+{
+    public int length;
+    public int width; 
+    public Color Color;
+    public Vehicle()
+    {
+        length=0;
+        width=0;
+        Color=null;
+    }
+    public Vehicle(int length,int width,Color Color){
+        this.length=length;
+        this.width=width;
+        this.Color=Color;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    //public int sampleMethod(int y)
+    {
+        // put your code here
+    }
+}
