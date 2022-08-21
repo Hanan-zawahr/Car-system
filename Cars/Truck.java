8cf9984c8fbc428ad3e5cd9ed178ff2dda0b6372
Hanan Zawahra\thwaib <hanan.zawahra@harri.com>
2022-08-22 00:42:28 +0200

    Create Truck class

---
+++ Cars/Truck.java
@@ -0,0 +1,26 @@
+public class Truck extends Vehicle
+{
+    public double freeWeight;
+    public double fullWight;
+    public Truck()
+    {
+        freeWeight=0;
+        fullWight=0;
+    }
+    public Truck(double freeWeight,double fullWight){
+        this.freeWeight=freeWeight;
+        this.fullWight=fullWight;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    //public int sampleMethod(int y)
+    {
+        // put your code here
+        
+    }
+}
