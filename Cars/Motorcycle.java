a40885497a45f82419f9d3c02e1b98ea8f34dce7
Hanan Zawahra\thwaib <hanan.zawahra@harri.com>
2022-08-22 00:22:28 +0200

    Create Motorcycle class

---
+++ Cars/Motorcycle.java
@@ -0,0 +1,25 @@
+public class Motorcycle extends Automobile
+{
+    public double tierdiameter;
+    public double length;
+    public Motorcycle()
+    {
+        tierdiameter=0;
+        length=0;
+    }
+    public Motorcycle(double tierdiameter,double length){
+        this.tierdiameter=tierdiameter;
+        this.length=length;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    //public int sampleMethod(int y)
+    {
+        // put your code here
+    }
+}
