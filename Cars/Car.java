a73c32a45d9852826736dfd1532aa87dd7b1b260
Hanan Zawahra\thwaib <hanan.zawahra@harri.com>
2022-08-22 00:38:05 +0200

    Create car class

---
+++ Cars/Car.java
@@ -0,0 +1,25 @@
+public class Car extends Vehicle
+{
+    public int chair_num;
+    public boolean is_furnitre_leather; 
+    public Car()
+    {
+        chair_num=0;
+        is_furnitre_leather=false;
+    }
+    public Car(int chair_num,boolean is_furnitre_leather){
+        this.chair_num=chair_num;
+        this.is_furnitre_leather=is_furnitre_leather;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    //public int sampleMethod(int y)
+    {
+        // put your code here
+    }
+}
