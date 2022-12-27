fe5f345efeb78f307faaba8b9fd9c5e955758dd4
Hanan Zawahra\thwaib <hanan.zawahra@harri.com>
2022-12-27 11:23:59 +0200

    edits

--- Cars/Main.java
+++ Cars/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-       
         Scanner in = new Scanner(System.in);
         ArrayList<Car> carList = new ArrayList<>();
         ArrayList<Motorcycle> motorcycleList = new ArrayList<>();
